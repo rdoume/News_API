c70fd47c4c81e8f6025ad84e3561505591d233a4
Robin Doumerc <robin@qwantresearch.com>
2018-06-12 16:01:54 +0200

    in which the hero deleted the CORS

--- news_api/app.py
+++ news_api/app.py
@@ -5,7 +5,7 @@ App runner
 # System imports
 # Third-party imports
 import falcon
-from falcon_cors import CORS
+#from falcon_cors import CORS
 
 # Local imports
 
@@ -21,11 +21,11 @@ clusters = TopClusters(db)
 
 
 # Create falcon app
-cors = CORS(allow_origins_list=["http://localhost:000"])
-public_cors = CORS(
-    allow_all_origins=True, allow_all_methods=True, allow_all_headers=True
-)
-app = falcon.API(middleware=[public_cors.middleware])
+#cors = CORS(allow_origins_list=["http://localhost:000"])
+#public_cors = CORS(
+#    allow_all_origins=True, allow_all_methods=True, allow_all_headers=True
+#)
+app = falcon.API()
 app.add_route("/v1/search", simplesearch)
 app.add_route("/v1/entities/", entity)
 app.add_route("/v1/clusters/", clusters)
